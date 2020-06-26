99e791f0110705a56c3ca4ecb0b64aa3e28cd380
Kieu Cong Hau <kieuconghau2000@gmail.com>
2020-06-26 22:28:02 +0700

    100% logout

--- server/Program.h
+++ server/Program.h
@@ -24,7 +24,9 @@ enum class SendMsgFlag : uint8_t
 	NEW_USER_LOGIN,
 	NEW_FILE_LIST,
 	NEW_FILE,
-	LOGOUT
+	LOGOUT_CLIENT,
+	LOGOUT_SERVER,
+	CLIENT_LOGOUT_NOTIF
 };
 
 enum class RcvMsgFlag : uint8_t
@@ -34,7 +36,8 @@ enum class RcvMsgFlag : uint8_t
 	PASSWORD,	// unused
 	UPLOAD_FILE,
 	DOWNLOAD_FILE,
-	LOGOUT
+	LOGOUT_CLIENT,
+	LOGOUT_SERVER
 };
 
 class Program
@@ -109,8 +112,10 @@ private:
 	void receiveAFileFromClient(std::string const& uploadFileName, User* user);
 
 	// Logout
-	void receiveALogoutRequestFromClient(User* user);	// user logout
-	void sendALogoutRequest();		// Server logout
+	void sendALogoutReply(User* user);	// user logout
+
+	void sendALogoutRequest();			// Server logout
+	void receiveALogoutReply();			// Server logout
 
 	// Handle error
 	void printLastError();
